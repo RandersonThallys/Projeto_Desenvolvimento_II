85d0ca1f7189eed38f476c08b30b16961e31f6ac
rtb <rtb@LAB-B-12.aeso.br>
2016-03-14 21:00:08 -0300

    Exception

--- Projeto1/src/br/aeso/aula08/exemplo/ClienteJaCadastradoException.java
+++ Projeto1/src/br/aeso/aula08/exemplo/ClienteJaCadastradoException.java
@@ -1,5 +1,19 @@
 package br.aeso.aula08.exemplo;
 
 public class ClienteJaCadastradoException {
+	
+	
+throw new ClienteJaCadastradoException("Clienta Ja Cadastrado");
+
+System.out.println("ERRO: Cliente Ja Cadastrado");
+
+
+	
+
+
+
+		
+	
+	
 
 }
